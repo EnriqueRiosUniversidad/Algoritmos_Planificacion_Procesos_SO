152f4d03c9c4f9f4f20c730c993dbb1f916a418e
enrique <enrique.rios2019@fiuni.edu.py>
2022-04-30 16:49:14 -0400

    first commit

---
+++ src/main/java/com/mycompany/algoritmo_planificacion_procesos/Algoritmo_Planificacion_Procesos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.algoritmo_planificacion_procesos;
+
+/**
+ *
+ * @author wwwal
+ */
+public class Algoritmo_Planificacion_Procesos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
