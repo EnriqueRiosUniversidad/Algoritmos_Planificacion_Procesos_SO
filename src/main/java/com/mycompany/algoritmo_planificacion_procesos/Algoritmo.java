13b6bea2e02fafa6d616da5fa1e117ef3db68d04
enrique <enrique.rios2019@fiuni.edu.py>
2022-05-09 10:04:32 -0400

    Se implemento la clase FCFS hereda de la clase mejorada Algoritmos

--- src/main/java/com/mycompany/algoritmo_planificacion_procesos/Algoritmo.java
+++ src/main/java/com/mycompany/algoritmo_planificacion_procesos/Algoritmo.java
@@ -1,42 +1,135 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package com.mycompany.algoritmo_planificacion_procesos;
 import java.util.ArrayList;
 
 public class Algoritmo {
 /*
-    Consiste en dos valores basicos, el conteo total de rafagas
-    y una lista donde se guardara una representacion de cada rafaga
-    donde cada proceso ocupara un lugar de acuerdo al algoritmo empleado
-    ej= [A,A,A,B,B,B,B,C,C,C,C]
-    
+Consiste en dos valores basicos, el conteo total de rafagas
+y una lista donde se guardara una representacion de cada rafaga
+donde cada proceso ocupara un lugar de acuerdo al algoritmo empleado
+ej= [A,A,A,B,B,B,B,C,C,C,C]
+
+    ----Nos permite tambien ordenar los arreglos de mayor a menor.
 */
-    public Algoritmo( int numero_Rafagas) {
-       this.numero_Rafagas = numero_Rafagas;
-       this.representacion_Rafagas = new ArrayList(numero_Rafagas);
+protected Algoritmo() {}
+/*
+private Algoritmo( int numero_Rafagas) {
+   this.numero_Rafagas = numero_Rafagas;
+   this.representacion_Rafagas = new ArrayList(numero_Rafagas);
+}*/
+
+public static void crear_lista_Vacia_Representacion(int numero_Rafagas){
+    Algoritmo.total_Rafagas = numero_Rafagas;
+    representacion_Rafagas = new ArrayList(numero_Rafagas);
+}
+
+//Retorna un arreglo ordenado de menor a mayor de acuerdo a sus 
+//RAFAGAS.
+public static ArrayList<Proceso> ord_Proc_Min_Rafagas(ArrayList<Proceso> procesos ){
+   // ArrayList<Proceso> temp = new ArrayList(procesos.size());
+   // temp = (ArrayList<Proceso>)procesos.clone(); //Por seguridad copio esta lista.
+    Proceso clave;
+    int j = 1;
+    for (int i=1; i< procesos.size(); i++){
+        clave=  procesos.get(i);
+
+        while( j>= 0  &&  procesos.get(j).getCantidad_De_Rafagas() > clave.getCantidad_De_Rafagas()  ){
+               procesos.set(j+1,procesos.get(j));
+                j--;
+        }
+
+        procesos.set(j+1, clave);
+
     }
+    return procesos;
+}
+
+//Tiempo de llegada
+//Retorna un arreglo ordenado de menor a mayor.
+
+public static ArrayList<Proceso> ord_Proc_Min_LLegada(ArrayList<Proceso> procesos){
+    Proceso aux;
+    for (int i = 0; i < procesos.size() - 1; i++) {
+            for (int j = 0; j < procesos.size() - i - 1; j++) {                                                              
+                if (procesos.get(j + 1).getTiempo_De_Llegada() < procesos.get(j).getTiempo_De_Llegada()) {
+                    aux = procesos.get(j + 1);
+                    procesos.set(j + 1, procesos.get(j));
+                    procesos.set(j, aux);             
+                }
+            }
+        }
     
-    public ArrayList<String> getRepresentacion_Rafagas() {
-        return representacion_Rafagas;
+    return procesos;
+}
+
+
+
+
+public static void sumar_rafagas(){
+    int suma=0;
+    for( int i=0; i< getProcesos().size();i++ ){
+        suma+= getProcesos().get(i).getCantidad_De_Rafagas();
+    }
+    Algoritmo.setTotal_Rafagas(suma);
+}
+
+
+public static ArrayList<Proceso> getProcesos() {
+        return procesos;
     }
 
-    public int getNumero_Rafagas() {
-        return numero_Rafagas;
-    }
+public static ArrayList<String> getRepresentacion_Rafagas() {
+    return representacion_Rafagas;
+}
 
-    public void setRepresentacion_Rafagas(ArrayList<String> representacion_Rafagas) {
-        this.representacion_Rafagas = representacion_Rafagas;
-    }
+public static int getTotal_Rafagas() {
+    return total_Rafagas;
+}
 
-    public void setNumero_Rafagas(int numero_Rafagas) {
-        this.numero_Rafagas = numero_Rafagas;
+public static int getCantidad_De_Procesos() {
+        return cantidad_De_Procesos;
+}
+
+public static void setRepresentacion_Rafagas(ArrayList<String> representacion_Rafagas) {
+    Algoritmo.representacion_Rafagas = representacion_Rafagas;
+}
+
+public static void setTotal_Rafagas(int numero_Rafagas) {
+    Algoritmo.total_Rafagas = numero_Rafagas;
+}
+
+public static void setCantidad_De_Procesos(int cantidad_De_Procesos) {
+        Algoritmo.cantidad_De_Procesos = cantidad_De_Procesos;
+}
+
+public static void setProcesos(ArrayList<Proceso> procesos) {
+    Algoritmo.procesos = procesos;
+}
+
+/*Cada que a;adimos un proceso, la lista de redimenciona
+y afecta los valores internos de esta clase.*/
+public static void addProceso(Proceso proceso){
+    procesos.add(proceso);
+    sumar_rafagas();
+    setCantidad_De_Procesos(procesos.size());
+    crear_lista_Vacia_Representacion(total_Rafagas);
+    procesos = ord_Proc_Min_LLegada_B(procesos);    
+}
+
+public static void imprimirRepresentacion(){
+    System.out.print(" | ");
+    for(int i=0; i<total_Rafagas ;i++){
+        System.out.print( representacion_Rafagas.get(i)+ " | ");
     }
-    
-    
-    private ArrayList<String> representacion_Rafagas;
-    private int numero_Rafagas;
-    private int cantidad_De_Procesos;
-    
+}
+
+private static ArrayList<String> representacion_Rafagas;
+private static ArrayList<Proceso> procesos = new ArrayList();
+private static int total_Rafagas;
+private static int cantidad_De_Procesos;
+
+
 }
--- src/main/java/com/mycompany/algoritmo_planificacion_procesos/FCFS.java
+++ src/main/java/com/mycompany/algoritmo_planificacion_procesos/FCFS.java
@@ -1,6 +1,55 @@
 
 package com.mycompany.algoritmo_planificacion_procesos;
+import java.util.ArrayList;
 
-public class FCFS {
+
+public class FCFS extends Algoritmo{
+    
+    public static void calcular(){
+        
+    }
+   /*Calcula la representacion 
+   * de procesos. 
+   * Los guarda en un string
+   */
+    public static void calcular_Representacion_Procesos(){
+        ArrayList<String> temp_representacion = getRepresentacion_Rafagas();
+        ArrayList<Proceso> procesos= getProcesos(); //solo para simplificar
+        int rafagas_Transcurridas=0;
+        if(FCFS.getCantidad_De_Procesos() > 0){
+            for(int proceso_n= 0; proceso_n < getCantidad_De_Procesos(); proceso_n++){
+                int tope = rafagas_Transcurridas + procesos.get(proceso_n).getCantidad_De_Rafagas();
+                while(rafagas_Transcurridas < tope){
+                    temp_representacion.add(procesos.get(proceso_n).getNombre());
+                    rafagas_Transcurridas++;
+                }
+            }
+            setRepresentacion_Rafagas(temp_representacion);
+        }else{
+            System.out.println("Numero de procesos 0, entrada invalida.");
+        }
+        
+    }
+    
+    
     
 }
+//Primero quiero ordenar mi lista de procesos segun orden de llegada.
+
+/*Lo que hace mi algoritmo es tener.
+-Lista de procesos.
+-lista de representacion.
+-numero de rafagas.
+-numero de procesos.
+
+Lo que hace
+-Ordenar de acuerdo a tiempo de llegada
+-Ordenar de acuerdo a cantidad de rafagas
+-Calcular la cantidad total de rafagas
+-addProceso, y recalcular las variables internas.
+-Imprimir su lista de representacion.
+*/
+
+/*Lo que hace FCFS
+-Rellenar la lista 'Representacion_procesos' con el nombre de cada proceso.
+*/
